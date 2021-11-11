8f3c6ba5348c67498a0cb421a1b16808cc69a88c
emv <emv@runtastic.com>
2021-11-11 15:21:58 +0100

    add null cleanser: draft

---
+++ spooq/transformer/null_cleaner.py
@@ -0,0 +1,168 @@
+import pyspark.sql.functions as F
+import pyspark.sql.types as sql_types
+from pyspark.sql.column import Column
+
+from .base_cleaner import BaseCleaner
+
+
+class NullCleaner(BaseCleaner):
+    """
+    Sets null values within a DataFrame to a default value.
+    Takes a dictionary with valid value ranges for each column to be cleaned.
+
+    Examples
+    --------
+    >>> from pyspark.sql import functions as F
+    >>> from spooq.transformer import ThresholdCleaner
+    >>> transformer = ThresholdCleaner(
+    >>>     thresholds={
+    >>>         "created_at": {
+    >>>             "min": 0,
+    >>>             "max": 1580737513,
+    >>>             "default": F.current_date()
+    >>>         },
+    >>>         "size_cm": {
+    >>>             "min": 70,
+    >>>             "max": 250,
+    >>>             "default": None
+    >>>         },
+    >>>     }
+    >>> )
+
+    >>> from spooq.transformer import ThresholdCleaner
+    >>> from pyspark.sql import Row
+    >>>
+    >>> input_df = spark.createDataFrame([
+    >>>     Row(id=0, integers=-5, some_factor=-0.75),
+    >>>     Row(id=1, integers= 5, some_factor= 1.25),
+    >>>     Row(id=2, integers=15, some_factor= 0.67),
+    >>> ])
+    >>> transformer = ThresholdCleaner(
+    >>>     thresholds={
+    >>>         "integers":    {"min":  0, "max": 10},
+    >>>         "some_factor": {"min": -1, "max":  1}
+    >>>     },
+    >>>     column_to_log_cleansed_values="cleansed_values_threshold",
+    >>>     store_as_map=True,
+    >>> )
+    >>> output_df = transformer.transform(input_df)
+    >>> output_df.show()
+    +---+--------+-----------+-------------------------+
+    | id|integers|some_factor|cleansed_values_threshold|
+    +---+--------+-----------+-------------------------+
+    |  0|    null|      -0.75|         [integers -> -5]|
+    |  1|       5|       null|     [some_factor -> 1...|
+    |  2|    null|       0.67|         [integers -> 15]|
+    +---+--------+-----------+-------------------------+
+    #  Remark: ``some_factor -> 1...`` from the df.show() is a bug, the stored value correctly holds ``"1.25"``
+    >>> output_df.printSchema()
+     |-- id: long (nullable = true)
+     |-- integers: long (nullable = true)
+     |-- some_factor: double (nullable = true)
+     |-- cleansed_values_threshold: map (nullable = false)
+     |    |-- key: string
+     |    |-- value: string (valueContainsNull = true)
+
+    Parameters
+    ----------
+    thresholds : :py:class:`dict`
+        Dictionary containing column names and respective valid ranges
+
+    column_to_log_cleansed_values : :any:`str`, Defaults to None
+        Defines a column in which the original (uncleansed) value will be stored in case of cleansing. If no column
+        name is given, nothing will be logged.
+
+    store_as_map : :any:`bool`, Defaults to False
+        Specifies if the logged cleansed values should be stored in a column as :any:`pyspark.sql.types.MapType` with
+        stringified values or as :any:`pyspark.sql.types.StructType` with the original respective data types.
+
+    Note
+    ----
+    Following cleansing rule attributes per column are supported:
+
+        * min, mandatory - any
+            A number or timestamp/date which serves as the lower limit for allowed values. Values
+            below this threshold will be cleansed.
+        * max, mandatory - any
+            A number or timestamp/date which serves as the upper limit for allowed values. Values
+            above this threshold will be cleansed.
+        * default, defaults to None - :class:`~pyspark.sql.column.Column` or any primitive Python value
+            If a value gets cleansed it gets replaced with the provided default value.
+
+    The :meth:`pyspark.sql.functions.between` method is used internally.
+
+    Returns
+    -------
+    :any:`pyspark.sql.DataFrame`
+        The transformed DataFrame
+
+    Raises
+    ------
+    :any:`exceptions.ValueError`
+        Threshold-based cleaning only supports Numeric, Date and Timestamp Types!
+        Column with name: {col_name} and type of: {col_type} was provided
+
+    Warning
+    -------
+    Only Numeric, TimestampType, and DateType data types are supported!
+    """
+
+    def __init__(self, cleaning_definitions={}, column_to_log_cleansed_values=None, store_as_map=False):
+        super().__init__(
+            cleaning_definitions=cleaning_definitions,
+            column_to_log_cleansed_values=column_to_log_cleansed_values,
+            store_as_map=store_as_map,
+            temporary_columns_prefix="dac28b56d8055953a7038bfe3b5097e7",
+        )
+        self.logger.debug("Range Definitions: " + str(self.cleaning_definitions))
+
+    def transform(self, input_df):
+        self.logger.debug("input_df Schema: " + input_df._jdf.schema().treeString())
+        ordered_column_names = input_df.columns
+
+        if self.column_to_log_cleansed_values:
+            column_names_to_clean = self.cleaning_definitions.keys()
+            temporary_column_names = self._get_temporary_column_names(column_names_to_clean)
+            input_df = self._add_temporary_columns(input_df, column_names_to_clean, temporary_column_names)
+
+        cleansing_expressions = []
+        for column_name, value_range in list(self.cleaning_definitions.items()):
+
+            data_type = input_df.schema[str(column_name)].dataType
+            substitute = value_range.get("default")
+
+            if substitute is None:
+                raise ValueError("Null-based cleaning requires the field default.", f"Default parameter is not specified for column with name: {column_name}")
+
+            if not isinstance(substitute, Column):
+                substitute = F.lit(substitute)
+
+            self.logger.debug("Ranges for column " + column_name + ": " + str(value_range))
+
+            cleansing_expression = (
+                F.when(
+                    F.col(column_name).isNull(),
+                    substitute,
+                ).otherwise(F.col(column_name))
+                .cast(data_type)
+            )
+            self.logger.debug("Cleansing Expression for " + column_name + ": " + str(cleansing_expression))
+            cleansing_expressions.append((column_name, cleansing_expression))
+
+        self.logger.info("Full null cleansing expression:")
+        self.logger.info(
+            ".".join(
+                [
+                    f"withColumn({column_name}, {str(cleansing_expr)})"
+                    for (column_name, cleansing_expr) in cleansing_expressions
+                ]
+            )
+        )
+        for (column_name, cleansing_expr) in cleansing_expressions:
+            input_df = input_df.withColumn(column_name, cleansing_expr)
+
+        if self.column_to_log_cleansed_values:
+            input_df = self._log_cleansed_values(input_df, column_names_to_clean, temporary_column_names)
+            ordered_column_names.append(self.column_to_log_cleansed_values)
+
+        return input_df.select(ordered_column_names)
---
+++ tests/unit/transformer/test_null_cleaner.py
@@ -0,0 +1,135 @@
+import pytest
+from pyspark.sql import Row
+from pyspark.sql import functions as F, types as T
+from chispa.dataframe_comparer import assert_df_equality
+from pyspark.sql.types import StringType, StructType, StructField
+from spooq.transformer.null_cleaner import NullCleaner
+
+
+@pytest.fixture(scope="class")
+def input_df(spark_session):
+    return spark_session.createDataFrame([Row(b="positive"), Row(b=None), Row(b="positive")])
+
+
+@pytest.fixture(scope="class")
+def input_df_for_tests_with_multiple_cleansing_rules(spark_session):
+    return spark_session.createDataFrame(
+        [
+            Row(a="stay", b="positive", c="or", d="healthy"),
+            Row(a="stay", b=None, c="and", d="healthy"),
+            Row(a="stay", b="positive", c=None, d="healthy"),
+        ]
+    )
+
+
+@pytest.fixture(scope="class")
+def cleansing_definitions_for_tests_with_multiple_cleansing_rules():
+    return dict(
+        b=dict(default="negative"),
+        c=dict(default="and"),
+    )
+
+
+class TestBasicAttributes(object):
+    """Basic attributes of the transformer instance"""
+
+    def test_has_logger(self):
+        assert hasattr(NullCleaner(), "logger")
+
+    def test_has_name(self):
+        assert NullCleaner().name == "NullCleaner"
+
+    def test_has_str_representation(self):
+        assert str(NullCleaner()) == "Transformer Object of Class NullCleaner"
+
+
+class TestExceptionsRaisedAndDefaultParametersApplied:
+    @pytest.fixture(scope="class")
+    def expected_output_df(self, spark_session):
+        return spark_session.createDataFrame([Row(b="positive"), Row(b=None), Row(b="positive")])
+
+    def test_missing_default_parameter(self, input_df):
+        """Missing default parameter"""
+        cleaning_definition = dict(b=dict())
+        with pytest.raises(ValueError) as excinfo:
+            NullCleaner(cleaning_definitions=cleaning_definition).transform(input_df)
+        assert "Null-based cleaning requires the field default." in str(
+            excinfo.value
+        )
+        assert "Default parameter is not specified for column with name: b" in str(excinfo.value)
+
+
+class TestCleaningOfNulls:
+
+    def tests_replacement_of_null_fields(self, spark_session):
+        """NullCleansed applies to values that are syntactically null (None) but not semantically null (eg. "")"""
+        input_df = spark_session.createDataFrame(
+            [
+                Row(id=1, status="active"),
+                Row(id=2, status=""),
+                Row(id=3, status="None"),
+                Row(id=4, status="inactive"),
+                Row(id=5, status=None),
+                Row(id=6, status="NULL"),
+            ]
+        )
+        expected_output_df = spark_session.createDataFrame(
+            [
+                Row(id=1, status="active"),
+                Row(id=2, status=""),
+                Row(id=3, status="None"),
+                Row(id=4, status="inactive"),
+                Row(id=5, status="REPLACEMENT_VALUE"),
+                Row(id=6, status="NULL"),
+            ]
+        )
+        cleaning_definition = dict(status=dict(default="REPLACEMENT_VALUE"))
+        output_df = NullCleaner(cleaning_definitions=cleaning_definition).transform(input_df)
+        assert_df_equality(expected_output_df, output_df)
+
+
+class TestCleansedValuesAreLoggedAsStruct:
+
+    def test_single_cleansing_rule_log_as_struct(self, input_df, spark_session):
+        schema = (
+            StructType([
+                StructField("b", StringType(), True),
+                StructField("cleansed_values_null", StructType([StructField("b", StringType(), True)]), False),
+            ])
+        )
+        expected_output_df = spark_session.createDataFrame(
+            [
+                Row(b="positive", cleansed_values_null=Row(b=None)),
+                Row(b="DEFAULT", cleansed_values_null=Row(b="null")),
+                Row(b="positive", cleansed_values_null=Row(b=None)),
+            ], schema=schema
+        )
+        cleansing_definitions = {"b": {"default": "DEFAULT"}}
+        output_df = NullCleaner(cleansing_definitions, column_to_log_cleansed_values="cleansed_values_null").transform(
+            input_df
+        )
+        assert_df_equality(expected_output_df, output_df)
+
+
+class TestCleansedValuesAreLoggedAsMap:
+
+    def test_single_cleansing_rule_log_as_map(self, input_df, spark_session):
+        expected_output_schema = T.StructType(
+            [
+                T.StructField("b", T.StringType(), True),
+                T.StructField("cleansed_values_null", T.MapType(T.StringType(), T.StringType(), True), True),
+            ]
+        )
+        expected_output_df = spark_session.createDataFrame(
+            [
+                ("positive", None),
+                ("DEFAULT", {"b": "null"}),
+                ("positive", None),
+            ],
+            schema=expected_output_schema,
+        )
+        cleansing_definitions = {"b": {"default": "DEFAULT"}}
+        output_df = NullCleaner(
+            cleansing_definitions, column_to_log_cleansed_values="cleansed_values_null", store_as_map=True
+        ).transform(input_df)
+        assert_df_equality(expected_output_df, output_df)
