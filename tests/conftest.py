603ca033083b142669d13bd43b159efb79921715
dae <david.hohensinn@runtastic.com>
2023-02-24 20:29:26 +0100

    some more import fix trials

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+
+from tests.helpers.spark_session import create_spark_session_for_tests
+
+
+@pytest.fixture(scope="session", autouse=True)
+def spark_session() -> SparkSession:
+    return create_spark_session_for_tests()
+
+
+@pytest.fixture(scope="session")
+def spark_context(spark_session: SparkSession) -> SparkContext:
+    return spark_session.sparkContext
