a16976d42313b4581a2c822e26390dbd56a8ac52
dae <david.hohensinn@runtastic.com>
2023-02-24 19:35:37 +0100

    switch to absolute imports in tests

--- tests/data/test_fixtures/__init__.py
+++ tests/data/test_fixtures/__init__.py
@@ -1,4 +1,4 @@
-from ...conftest import create_spark_session_for_tests
+from tests.conftest import create_spark_session_for_tests
 
 # # warm up spark_session to be able to import Spark functions
 create_spark_session_for_tests()
--- tests/helpers/skip_conditions.py
+++ tests/helpers/skip_conditions.py
@@ -1,5 +1,5 @@
 import pytest
-from ..conftest import create_spark_session_for_tests
+from tests.conftest import create_spark_session_for_tests
 
 # # warm up spark_session to be able to import Spark functions
 spark_session = create_spark_session_for_tests()
